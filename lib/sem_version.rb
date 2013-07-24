47699722378e9c383df9ffbe72ad2b0ec4fdc171
Antony Male <antony.male@gmail.com>
2013-07-24 13:09:53 +0100

    Bump version to 2.0.0

--- lib/sem_version.rb
+++ lib/sem_version.rb
@@ -1,7 +1,7 @@
 class SemVersion
   include Comparable
 
-  VERSION = '1.3.0'
+  VERSION = '2.0.0'
 
   # Pattern allows min and patch to be skipped. We have to do extra checking if we want them
   SEMVER_REGEX = /^(\d+)(?:\.(\d+)(?:\.(\d+)(?:-([\dA-Za-z\-]+(?:\.[\dA-Za-z\-]+)*))?(?:\+([\dA-Za-z\-]+(?:\.[\dA-Za-z\-]+)*))?)?)?$/
